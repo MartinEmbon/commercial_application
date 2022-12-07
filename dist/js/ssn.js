c50a7c283fc6e46435313a8c67dd9ab0916aa2c3
Martin Embon <martinembon@hotmail.com>
2022-12-06 22:36:34 -0300

    node

--- dist/js/ssn.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  
-    $.getScript('https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js', function () {
-  
-      $(".ssn input").mask("999-99-9999");
-  
-    });
-  
-  });
