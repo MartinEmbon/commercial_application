ea4ce52a41f8b9fecb4d480ef2a164efa73fe0f4
Martin Embon <martinembon@hotmail.com>
2022-12-08 16:38:35 -0300

    pre multer

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,11 @@
 require("dotenv").config()
 const nodemailer = require("nodemailer")
-
+const fs = require("fs")
 let userController = {
-    index:(req,res)=> {
+    index: (req, res) => {
         return res.render("index")
     },
-    email:(req,res)=> {
+    email: (req, res) => {
         let transporter = nodemailer.createTransport({
             service: 'gmail',
             host: 'smpt.gmail.com',
@@ -15,22 +15,34 @@ let userController = {
                 pass: "tdwyqeyddeqcymjr"
             }
         });
-        let mailOptions = {        
-            from:req.body.email,             
-            to:'martinembon@hotmail.com' ,
-            subject:`Message from ${req.body.name}`,
-            text: `${req.body.message}`
-        
+        let mailOptions = {
+            from: req.body.email,
+            to: 'martinembon@hotmail.com',
+            subject: `Message from ${req.body.name}`,
+            text: `${req.body.message}`,
+            attachments: [{
+                'filename': 'generated.pdf',
+                'path':"./public/generated.pdf",
+                'contentType':'application/pdf'
+            }]
+
+            // attachments: [{
+            //     filename: req.body.file,
+
+            //     //content: 'pdf',
+            //     //contentType: 'application/pdf'
+            // }]
+
         };
         transporter.sendMail(mailOptions, (err, data) => {
             if (err) {
-                console.log('Error al enviar',err);
+                console.log('Error al enviar', err);
             } else {
-            console.log('Email enviado!!!');
+                console.log('Email enviado!!!');
             }
         });
-        return res.redirect("/")        
-    }            
+        return res.redirect("/")
+    }
 }
 
 module.exports = userController
