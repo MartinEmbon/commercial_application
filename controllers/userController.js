77b8b9f2f5ad3459a7484e71881219fb16c2e2f2
Martin Embon <martinembon@hotmail.com>
2022-12-08 21:00:44 -0300

    multer ok

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,50 @@
 require("dotenv").config()
 const nodemailer = require("nodemailer")
+var multer  = require('multer');
+var fileUpload= require('../upload-middleware');
+
+
 
 let userController = {
+    fileUploadForm:function(req,res){
+        res.render('index');
+     },
+     uploadFile:function(req,res){
+        var upload = multer({
+                    storage: fileUpload.files.storage(), 
+                    allowedFile:fileUpload.files.allowedFile 
+                    }).single('file');
+        upload(req, res, function (err) {
+           if (err instanceof multer.MulterError) {
+              res.send(err);
+           } else if (err) {
+              res.send(err);
+           }else{
+              res.render('index');
+           }
+           
+        })
+        
+     },    
     index: (req, res) => {
         return res.render("index")
     },
     email: (req, res) => {
+        const files = req.files
+            var upload = multer({
+                        storage: fileUpload.files.storage(), 
+                        allowedFile:fileUpload.files.allowedFile 
+                        }).single('file'); upload(req, res, function (err) {
+                            if (err instanceof multer.MulterError) {
+                               res.send(err);
+                            } else if (err) {
+                               res.send(err);
+                            }else{
+                               res.render('index');
+                            }
+                            
+                         })
+        
         let transporter = nodemailer.createTransport({
             service: 'gmail',
             host: 'smpt.gmail.com',
@@ -21,9 +60,8 @@ let userController = {
             subject: `Message from ${req.body.name}`,
             text: `${req.body.message}`,
             attachments: [{
-                filename:  req.file,
-                // path: req.file.path,
-                path: './public/',
+                filename: req.body.file,
+                path: `uploads/generated (3).pdf`,
                 'contentType':'application/pdf'
             }]
 
--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const userController = require("../controllers/userController")
+//const fileUploadController= require('../upload-middleware')
 
-const multer = require("multer");
-const fs = require("fs")
-
-const Storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-      callback(null, "./public");
-    },
-    filename: function (req, file, callback) {
-      callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
-    },
-  });
-
-/** usando configuração como storage do multer */
-const attachmentUpload = multer({
-    storage: Storage,
-  }).single("attachment");
-  
 router.get("/",  userController.index)
-router.post("/", attachmentUpload,userController.email)
+router.post("/", userController.email)
+//router.post("/",userController.uploadFile, userController.email)
 
 module.exports = router
